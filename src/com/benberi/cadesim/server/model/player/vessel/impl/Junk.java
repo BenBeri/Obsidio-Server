d6ded164fcafe74ce49c794b14567a78cda36b29
Maarten Desktop <jongen_cool@hotmail.com>
2018-01-11 15:14:44 +0100

    Forgot a file in junk implementation commit

---
+++ src/com/benberi/cadesim/server/model/player/vessel/impl/Junk.java
@@ -0,0 +1,58 @@
+package com.benberi.cadesim.server.model.player.vessel.impl;
+
+
+import com.benberi.cadesim.server.model.player.Player;
+import com.benberi.cadesim.server.model.player.vessel.CannonType;
+import com.benberi.cadesim.server.model.player.vessel.Vessel;
+
+public class WarBrig extends Vessel {
+
+    public WarBrig(Player p) {
+        super(p);
+    }
+
+    @Override
+    public int getID() {
+        return 5;
+    }
+
+    @Override
+    public int getSize() {
+        return 2;
+    }
+
+    @Override
+    public int getInfluenceDiameter() {
+        return 4;
+    }
+
+    @Override
+    public int getMaxCannons() {
+        return 12;
+    }
+
+    @Override
+    public boolean isDualCannon() {
+        return false;
+    }
+
+    @Override
+    public boolean isManuaver() {
+        return true;
+    }
+
+    @Override
+    public double getMaxDamage() {
+        return 16.66;
+    }
+
+    @Override
+    public double getRamDamage() {
+        return 1;
+    }
+
+    @Override
+    public CannonType getCannonType() {
+        return CannonType.LARGE;
+    }
+}
